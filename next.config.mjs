c9b049da26b3d310bbe68f6b49211790e1a22499
tesslaray <cristian.valdivia@alumnos.usm.cl>
2025-01-07 01:56:07 -0300

    feat: change in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.cache = false;
+    }
+
     config.resolve.fallback = { fs: false, net: false, tls: false };
     config.externals.push("pino-pretty", "encoding");
     return config;
